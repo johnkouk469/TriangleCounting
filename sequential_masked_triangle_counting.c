599bd1f9b3b240095ada78aed082203c188d9d7d
johnkouk469 <37601899+johnkouk469@users.noreply.github.com>
2021-11-14 07:46:35 +0000

    SIGSEGV

--- sequential_masked_triangle_counting.c
+++ sequential_masked_triangle_counting.c
@@ -1,4 +1,6 @@
 /*
+** v4.c -- version 4 of Vertexwise triangle counting
+**
 ** Loading sparse adjacency matrix using the Matrix Market format
 ** and converting the martix from COO to CSC
 **
@@ -8,12 +10,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 #include <time.h>
 #include "mmio.h"
 
-void quicksort(uint32_t element_list[], uint32_t low, uint32_t high){
-	uint32_t pivot, value1, value2, temp;
+void quicksort(int element_list[], int low, int high){
+	int pivot, value1, value2, temp;
 	if (low < high){
 		pivot = low;
 		value1 = low;
@@ -40,44 +41,44 @@ void quicksort(uint32_t element_list[], uint32_t low, uint32_t high){
 }
 
 void coo2csc(
-    uint32_t       * const row,       /*!< CSC row start indices */
-    uint32_t       * const col,       /*!< CSC column indices */
-    uint32_t const * const row_coo,   /*!< COO row indices */
-    uint32_t const * const col_coo,   /*!< COO column indices */
-    uint32_t const         nnz,       /*!< Number of nonzero elements */
-    uint32_t const         n,         /*!< Number of rows/columns */
-    uint32_t const         isOneBased /*!< Whether COO is 0- or 1-based */
+    int       * const row,       /*!< CSC row start indices */
+    int       * const col,       /*!< CSC column indices */
+    int const * const row_coo,   /*!< COO row indices */
+    int const * const col_coo,   /*!< COO column indices */
+    int const         nnz,       /*!< Number of nonzero elements */
+    int const         n,         /*!< Number of rows/columns */
+    int const         isOneBased /*!< Whether COO is 0- or 1-based */
 ) {
 
     // ----- cannot assume that input is already 0!
-    for (uint32_t l = 0; l < n+1; l++) col[l] = 0;
+    for (int l = 0; l < n+1; l++) col[l] = 0;
 
     // ----- find the correct column sizes
-    for (uint32_t l = 0; l < nnz; l++)
+    for (int l = 0; l < nnz; l++)
         col[col_coo[l] - isOneBased]++;
 
     // ----- cumulative sum
-    for (uint32_t i = 0, cumsum = 0; i < n; i++){
-        uint32_t temp = col[i];
+    for (int i = 0, cumsum = 0; i < n; i++){
+        int temp = col[i];
         col[i] = cumsum;
         cumsum += temp;
     }
     col[n] = nnz;
 
     // ----- copy the row indices to the correct place
-    for (uint32_t l=0; l < nnz; l++){
-        uint32_t col_l;
+    for (int l=0; l < nnz; l++){
+        int col_l;
         col_l = col_coo[l] - isOneBased;
 
-        uint32_t dst = col[col_l];
+        int dst = col[col_l];
         row[dst] = row_coo[l] - isOneBased;
 
         col[col_l]++;
     }
 
     // ----- revert the column pointers
-    for (uint32_t i=0, last=0; i<n; i++){
-        uint32_t temp = col[i];
+    for (int i=0, last=0; i<n; i++){
+        int temp = col[i];
         col[i] = last;
         last = temp;
     }
@@ -86,11 +87,11 @@ void coo2csc(
 
 int main(int argc, char *argv[]){
 
-    uint32_t ret_code;
+    int ret_code;
     MM_typecode matcode;
     FILE *f;
-    uint32_t M, N, nnz;
-    uint32_t *coo_row, *coo_col;
+    int M, N, nnz;
+    int *coo_row, *coo_col;
     double *val;
 
     if (argc < 2)
@@ -124,21 +125,21 @@ int main(int argc, char *argv[]){
 
     /* reseve memory for matrices */
 
-    coo_row = (uint32_t *) malloc(nnz * sizeof(uint32_t));
-    coo_col = (uint32_t *) malloc(nnz * sizeof(uint32_t));
+    coo_row = (int *) malloc(nnz * sizeof(int));
+    coo_col = (int *) malloc(nnz * sizeof(int));
     val = (double *) malloc(nnz * sizeof(double));
 
     /* Replace missing val column with 1s and change the fscanf to match patter matrices*/
 
     if (!mm_is_pattern(matcode)){
 
-        for (uint32_t i=0; i<nnz; i++)
+        for (int i=0; i<nnz; i++)
             fscanf(f, "%d %d %lg\n", &coo_row[i], &coo_col[i], &val[i]);
 
     }
     else{
 
-        for (uint32_t i=0; i<nnz; i++){
+        for (int i=0; i<nnz; i++){
             fscanf(f, "%d %d\n", &coo_row[i], &coo_col[i]);
             val[i]=1;
         }
@@ -146,11 +147,11 @@ int main(int argc, char *argv[]){
 
     if (f !=stdin) fclose(f);
 
-    uint32_t *cooFull_row = (uint32_t *) malloc((2*nnz)*sizeof(uint32_t));
-    uint32_t *cooFull_col = (uint32_t *) malloc((2*nnz)*sizeof(uint32_t));
-    uint32_t *valFull = (uint32_t *) malloc((2*nnz)*sizeof(uint32_t));
+    int *cooFull_row = (int *) malloc((2*nnz)*sizeof(int));
+    int *cooFull_col = (int *) malloc((2*nnz)*sizeof(int));
+    int *valFull = (int *) malloc((2*nnz)*sizeof(int));
 
-    for(uint32_t i=0; i<nnz; i++){
+    for(int i=0; i<nnz; i++){
         cooFull_row[i] = coo_row[i];
         cooFull_row[nnz+i] = coo_col[i];
         cooFull_col[i] = coo_col[i];
@@ -172,8 +173,8 @@ int main(int argc, char *argv[]){
     free(coo_col);
     free(val);
 
-    uint32_t *csc_row = (uint32_t *)malloc((2*nnz)*sizeof(uint32_t));
-    uint32_t *csc_col = (uint32_t *)malloc((N+1)*sizeof(uint32_t));
+    int *csc_row = (int *)malloc((2*nnz)*sizeof(int));
+    int *csc_col = (int *)malloc((N+1)*sizeof(int));
 
     coo2csc(csc_row, csc_col,
             cooFull_row, cooFull_col,
@@ -182,11 +183,11 @@ int main(int argc, char *argv[]){
     free(cooFull_row);
     free(cooFull_col);
 
-    for(uint32_t i=0; i<N+1; i++){
+    for(int i=0; i<N+1; i++){
         quicksort(csc_row, csc_col[i], csc_col[i+1]-1);
     }
 
-    /*
+    
     printf("csc_col: ");
     for(int i=0; i<N+1; i++)
         printf("%d ", csc_col[i]);
@@ -194,11 +195,12 @@ int main(int argc, char *argv[]){
     for(int i=0; i<2*nnz; i++)
         printf("%d ", csc_row[i]);
     printf("\n");
-    */
+    printf("nnz: %d\n", nnz);
+    
 
-    uint32_t c3[N];
+    int c3[N];
 
-    for(uint32_t i=0; i<N; i++)
+    for(int i=0; i<N; i++)
         c3[i]=0;
 
 
@@ -208,49 +210,45 @@ int main(int argc, char *argv[]){
 
     clock_gettime(CLOCK_MONOTONIC, &start);
 
-	for(uint32_t i=0; i<N+1; i++){ // i the rows of matrix 1
-        uint32_t sum = 0;
-        if(csc_col[i+1] > csc_col[i]){
-            uint32_t ioc1[csc_col[i+1]-csc_col[i]];
-            for(uint32_t k=csc_col[i]; k<csc_col[i+1]; k++)
-                ioc1[k-csc_col[i]] = csc_row[k]; // list of the column indices for row i of the non zero values of matrix 1
-
-            for(uint32_t j=0; j<N+1; j++){ // j the columns of matrix 2
-                if(csc_col[j+1] > csc_col[j]){
-
-                    uint32_t ior2[csc_col[j+1]-csc_col[j]];
-                    for(uint32_t w=csc_col[j]; w<csc_col[j+1]; w++)
-                        ior2[w-csc_col[j]] = csc_row[w]; // list of the row indices for column j of the non zero values of matrix 2
-
-                    uint32_t common = 0;
-                    uint32_t flag = 0;
-                    for(uint32_t l=0; l<csc_col[i+1]-csc_col[i]; l++){
-                        uint32_t counter = 0;
-                        while((counter + flag) < (csc_col[j+1]-csc_col[j])){
-                            if(ioc1[l] < ior2[counter+flag]){
-                                counter++;
-                                break;
-                            }else if(ioc1[l] == ior2[counter+flag]){
-                                common++;
-                                break;
-                            }else
-                                flag++;
+	for(int j=0; j<N+1; j++){ // i the rows of matrix 1
+        for(int i=csc_col[j]; i<csc_col[j+1]; i++){
+            
+            // if((csc_col[i+1] > csc_col[i]) && (csc_col[j+1]>csc_col[j])){
+                // create array with the indexes to the rows of the nonzero values for
+                // row i of matrix 1 in the multiplication A*A
+                int nzrangeI = csc_col[i+1]-csc_col[i];
+                int rowOfMatrix1[nzrangeI];
+                for(int k=csc_col[i]; k<csc_col[i+1]; k++){
+                    printf("%d\n",k);
+                    rowOfMatrix1[k-csc_col[i]] = csc_row[k];
+                }
+                // // // create array with the indexes to the rows of the nonzero values for
+                // // // column j of matrix 2 in the multiplication A*A
+                int nzrangeJ = csc_col[j+1]-csc_col[j];
+                int columnOfMatrix2[nzrangeJ];
+                for(int l=csc_col[j]; l<csc_col[j+1]; l++){
+                    printf("%d\n",l);
+                    columnOfMatrix2[l-csc_col[j]] = csc_row[l];
+                }
+                printf("nzrangeI: %d, nzrangeJ: %d\n", nzrangeI, nzrangeJ);
+                for(int w=0; w<nzrangeI; w++){
+                    printf("rowOfMatrix1[w]: %d\n",rowOfMatrix1[w]);
+                    for(int y=0; y<nzrangeJ;y++){
+                        printf("w: %d, rowOfMatrix1[w]: %d, columnOfMatrix2[y]: %d, y: %d\n",w,rowOfMatrix1[w],columnOfMatrix2[y],y);
+                        if(rowOfMatrix1[w]<columnOfMatrix2[y]){
+                            break;
                         }
-                    }
-                    // Found A^2(i,j)=common
-                    if(common != 0){
-                        for(uint32_t k=csc_col[i]; k<csc_col[i+1]; k++){
-                            if(csc_row[k] == j){
-                                sum += common;
-                                break;
-                            }
+                        if (rowOfMatrix1[w] == columnOfMatrix2[y]){
+                            c3[j]++;
+                            printf("common\n");
+                            break;
                         }
                     }
                 }
-            }
+            // } 
+            printf("%d, ",i);
         }
-        c3[i] = sum;
-
+        printf("\n j \n");
     }
 
     clock_gettime(CLOCK_MONOTONIC, &stop);
@@ -259,7 +257,7 @@ int main(int argc, char *argv[]){
     free(csc_col);
 
     printf("\nC3:\n");
-    for(uint32_t i=0; i<N; i++){
+    for(int i=0; i<N; i++){
         if(c3[i]%2 != 0)
             c3[i]++;
         c3[i] = c3[i]/2;
